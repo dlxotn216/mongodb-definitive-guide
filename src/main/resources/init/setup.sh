898c7262476b8aa0281704e543a83e2ed21525f9
taesu <taesulee93@gmail.com>
2022-10-15 14:24:58 +0900

    Spring Data Mongodb 연동 설정 버전
    이설정이 맞는 듯

--- src/main/resources/init/setup.sh
+++ src/main/resources/init/setup.sh
@@ -1,3 +1,3 @@
 #init/setup.sh
-sleep 5 | echo "Waiting for the servers to start..."
+sleep 15 | echo "Waiting for the servers to start..."
 mongosh mongodb://localhost:27017 /usr/src/configs/init/setReplication.js
--- src/main/resources/setup.js
+++ src/main/resources/setup.js
@@ -1,10 +1,9 @@
 rsconf = {
   _id: 'mdbdefGuide',
   members: [
-    {_id:  0, host:  'mongo-1:27017'},
-    {_id:  1, host:  'mongo-2:27017'},
-    {_id:  2, host:  'mongo-3:27017'},
+    {_id:  0, host:  'mongo-1:27017', priority: 2},
+    {_id:  1, host:  'mongo-2:27017', priority: 1},
+    {_id:  2, host:  'mongo-3:27017', priority: 1},
   ]
 }
-rs.initiate(rsconf)
-rs.conf();
+rs.initiate(rsconf);
